e5532d6447e078b72ba3bb32be29656ee9b0d167
Rohov <rogov.dmitry@gmail.com>
2018-03-28 17:58:34 +0300

    ➕

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 
+import { Link, Route, BrowserRouter as Router } from 'react-router-dom';
 import React, { Component } from 'react';
 import { withProps, withState } from 'recompose';
 
@@ -23,49 +24,54 @@ const Container = ({ children }) => (
 class App extends Component {
   render() {
     return (
-      <Container>
-        <h1>Wheel Of Life v0.0.1</h1>
-
-        <Question
-          block={this.props.blocks[this.props.index]}
-          onChange={value => {
-            console.log(value);
-
-            this.props.setBlocks([
-              ...this.props.blocks.slice(0, this.props.index),
-              {
-                ...this.props.blocks[this.props.index],
-                value: value
-              },
-              ...this.props.blocks.slice(this.props.index + 1, this.props.blocks.length)
-            ]);
-          }}
+      <Router>
+        <Route
+          path={'*'}
+          render={({ match }) => (
+            <Container>
+              <h1>Wheel Of Life v0.0.1</h1>
+              {match.url}
+              <Question
+                block={this.props.blocks[this.props.index]}
+                onChange={value => {
+                  this.props.setBlocks([
+                    ...this.props.blocks.slice(0, this.props.index),
+                    {
+                      ...this.props.blocks[this.props.index],
+                      value: value
+                    },
+                    ...this.props.blocks.slice(this.props.index + 1, this.props.blocks.length)
+                  ]);
+                }}
+              />
+              <br />
+              <button
+                onClick={() => this.props.setIndex(this.props.index - 1)}
+                disabled={this.props.index === 0}
+              >
+                prev
+              </button>
+              <button
+                onClick={() => this.props.setIndex(this.props.index + 1)}
+                disabled={this.props.index === this.props.blocks.length - 1}
+              >
+                skip
+              </button>
+              <button
+                onClick={() => this.props.setIndex(this.props.index + 1)}
+                disabled={this.props.index === this.props.blocks.length - 1}
+              >
+                next
+              </button>
+              <br />
+              <h2>Result</h2>
+              <Wheel blocks={this.props.blocks} />
+              <br />
+              <button>Share</button>
+            </Container>
+          )}
         />
-        <br />
-        <button
-          onClick={() => this.props.setIndex(this.props.index - 1)}
-          disabled={this.props.index === 0}
-        >
-          prev
-        </button>
-        <button
-          onClick={() => this.props.setIndex(this.props.index + 1)}
-          disabled={this.props.index === this.props.blocks.length - 1}
-        >
-          skip
-        </button>
-        <button
-          onClick={() => this.props.setIndex(this.props.index + 1)}
-          disabled={this.props.index === this.props.blocks.length - 1}
-        >
-          next
-        </button>
-        <br />
-        <h2>Result</h2>
-        <Wheel blocks={this.props.blocks} />
-        <br />
-        <button>Share</button>
-      </Container>
+      </Router>
     );
   }
 }
---
+++ src/common/helpers.js
@@ -0,0 +1,7 @@
+export const encodeStateToURL = state => {
+  return 'ENCODED_STATE';
+};
+
+export const decodeStateFromURL = url => {
+  return {};
+};
