43fb9e392353f65617bb2dc073eb0c5135e4d39b
Rohov <rogov.dmitry@gmail.com>
2018-03-29 15:38:06 +0300

    ➕

--- src/features/Wheel/index.js
+++ src/features/Wheel/index.js
@@ -1,45 +1,51 @@
 import { compose, flow, map } from 'lodash/fp';
 
+import { OrdinalFrame } from 'semiotic';
 import React from 'react';
+import { SAMPLE_WIND_ROSE_DATA } from './constants';
 import { setDisplayName } from 'recompose';
 
 const mapPropsToQueries = () => [];
 
+const convertBlocksToWindRoseData = blocks => {
+  const STEP = 360 / blocks.length;
+  return flow(
+    map(block => ({
+      label: block.name,
+      angle: `${STEP * blocks.indexOf(block)} - ${STEP * (blocks.indexOf(block) + 1)}`,
+      value: block.value
+    }))
+  )(blocks);
+};
+
+const colorHash = {
+  Health: 'red',
+  Carreer: 'blue',
+  Fun: 'orange',
+  Friends: 'white',
+  Family: 'yellow',
+  'Personal development': 'grey',
+  Spirituality: 'purple',
+  Environment: 'green',
+  undefined: 'black'
+};
+
 const Component = ({ blocks }) => (
-  <div
-    style={{
-      border: '1px solid black'
+  <OrdinalFrame
+    size={[400, 400]}
+    data={convertBlocksToWindRoseData(blocks)}
+    oAccessor={'label'}
+    rAccessor={'value'}
+    style={d => ({ fill: colorHash[d.label] })}
+    type={'bar'}
+    projection={'radial'}
+    axis={{
+      label: { name: 'Windiness', locationDistance: 5 }
     }}
-  >
-    {flow(
-      map(block => (
-        <div
-          key={block.name}
-          style={{
-            opacity: (block.value + 1) / 10
-          }}
-        >
-          <div
-            style={{
-              width: '80%',
-              display: 'inline-block'
-            }}
-          >
-            {block.name}
-          </div>
-          <div
-            style={{
-              width: '20%',
-              display: 'inline-block',
-              textAlign: 'right'
-            }}
-          >
-            {block.value}
-          </div>
-        </div>
-      ))
-    )(blocks)}
-  </div>
+    oPadding={1}
+    margin={{ bottom: 20, top: 20, left: 20, right: 20 }}
+    hoverAnnotation={true}
+  />
 );
 
 const mapStateToProps = state => {
