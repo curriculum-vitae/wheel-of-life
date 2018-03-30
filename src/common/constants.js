253124b3dbb1f9981e1bc1ac26673f70aac5e0f1
Rohov <rogov.dmitry@gmail.com>
2018-03-30 15:16:07 +0300

    🛠 change default value of value

--- src/common/constants.js
+++ src/common/constants.js
@@ -6,7 +6,6 @@ import {
   deepOrange,
   green,
   indigo,
-  orange,
   pink,
   purple,
   red,
@@ -19,57 +18,57 @@ export const BLOCKS = [
   {
     name: 'Health',
     color: red[SHADE],
-    value: 1
+    value: 0
   },
   {
     name: 'Family',
     color: blue[SHADE],
-    value: 1
+    value: 0
   },
   {
     name: 'Carreer',
     color: pink[SHADE],
-    value: 1
+    value: 0
   },
   {
     name: 'Personal development',
     color: yellow[SHADE],
-    value: 1
+    value: 0
   },
 
   {
     name: 'Partner',
     color: purple[SHADE],
-    value: 1
+    value: 0
   },
   {
     name: 'Spirituality',
     color: deepOrange[SHADE],
-    value: 1
+    value: 0
   },
   {
     name: 'Fun',
     color: brown[SHADE],
-    value: 1
+    value: 0
   },
   {
     name: 'Environment',
     color: green[SHADE],
-    value: 1
+    value: 0
   },
   {
     name: 'Finances',
     color: cyan[SHADE],
-    value: 1
+    value: 0
   },
   {
     name: 'Friends',
     color: amber[SHADE],
-    value: 1
+    value: 0
   },
   {
     name: 'Social',
     color: indigo[SHADE],
-    value: 1
+    value: 0
   }
 ];
