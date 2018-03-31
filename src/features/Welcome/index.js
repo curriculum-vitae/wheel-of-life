f9914b728ce6c8334d9980ecc91f3da756dc68b8
Rohov <rogov.dmitry@gmail.com>
2018-03-31 16:21:39 +0300

    ➕ add two columns to main page

--- src/features/Welcome/index.js
+++ src/features/Welcome/index.js
@@ -1,6 +1,15 @@
-import { Button } from 'semantic-ui-react'
+import { Button, Grid } from 'semantic-ui-react'
+
 import { Link } from 'react-router-dom'
 import React from 'react'
+
+const Point = ({ title, text }) => (
+  <div>
+    <h1>{title}</h1>
+    <p>{text}</p>
+  </div>
+)
+
 export default () => (
   <div>
     <h1 style={{ textAlign: 'center', fontSize: '60px' }}>Well hello</h1>
@@ -11,5 +20,13 @@ export default () => (
         <Button>Start</Button>
       </Link>
     </div>
+    <Grid columns={1}>
+      <Grid.Column>
+        <Point title={'What is here?'} text={'A tool to improve your life'} />
+      </Grid.Column>
+      <Grid.Column>
+        <Point title={'Why is this?'} text={'It just 1 minute.'} />
+      </Grid.Column>
+    </Grid>
   </div>
 )
