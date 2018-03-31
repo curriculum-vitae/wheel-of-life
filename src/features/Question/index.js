34e2159ccdd036fe2a3546f5b6d1230dbc35ce54
Rohov <rogov.dmitry@gmail.com>
2018-03-31 16:26:29 +0300

    🎨 nice rate buttons

--- src/features/Question/index.js
+++ src/features/Question/index.js
@@ -1,52 +1,37 @@
-import { flow, times } from 'lodash/fp';
-
-import React from 'react';
-
-const ButtonForRating = ({ selected, ...props }) => (
-  <button
-    style={{
-      cursor: 'pointer',
-      width: '40px',
-      height: '40px',
-      backgroundColor: selected ? 'purple' : undefined,
-      color: selected ? 'white' : undefined
-    }}
-    {...props}
-  />
-);
+import { Button } from 'semantic-ui-react'
+import React from 'react'
+import { times } from 'lodash/fp'
 
 export default ({ block, onChange = () => {} }) => (
   <div
     style={{
-      padding: '50px'
-    }}
-  >
+      padding: '50px',
+    }}>
     <h2
       style={{
         textAlign: 'center',
         color: 'white',
         fontWeight: '100',
-        fontSize: '48px'
-      }}
-    >
+        fontSize: '48px',
+      }}>
       {block.name}
     </h2>
 
     <div
       style={{
         display: 'flex',
-        justifyContent: 'center'
-      }}
-    >
-      {times(index => (
-        <ButtonForRating
-          selected={block.value === index + 1}
-          key={index + 1}
-          onClick={() => onChange(index + 1)}
-        >
-          {index + 1}
-        </ButtonForRating>
-      ))(10)}
+        justifyContent: 'center',
+      }}>
+      <Button.Group size={'large'}>
+        {times(index => (
+          <Button
+            primary={block.value === index + 1}
+            key={index + 1}
+            onClick={() => onChange(index + 1)}>
+            {index + 1}
+          </Button>
+        ))(10)}
+      </Button.Group>
     </div>
   </div>
-);
+)
