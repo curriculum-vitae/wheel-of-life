ecdb214c0c5748ece1f6674ff439a1141cafb378
Rohov <rogov.dmitry@gmail.com>
2018-03-31 16:38:52 +0300

    ➖

--- src/features/Progress/index.js
+++ src/features/Progress/index.js
@@ -2,5 +2,9 @@ import { Progress } from 'semantic-ui-react'
 import React from 'react'
 
 export default ({ blocks, index }) => (
-  <Progress progress percent={Math.floor(100 * index / blocks.length)} />
+  <Progress
+    success
+    progress
+    percent={Math.floor(100 * index / blocks.length)}
+  />
 )
