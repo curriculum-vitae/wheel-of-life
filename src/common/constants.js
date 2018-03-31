f44401fd9c128cdbea1df1d3354b5241996ec86f
Rohov <rogov.dmitry@gmail.com>
2018-03-31 16:40:27 +0300

    💄

--- src/common/constants.js
+++ src/common/constants.js
@@ -9,66 +9,66 @@ import {
   pink,
   purple,
   red,
-  yellow
-} from 'common/colors';
+  yellow,
+} from 'common/colors'
 
-const SHADE = 700;
+const SHADE = 700
 
 export const BLOCKS = [
   {
     name: 'Health',
     color: red[SHADE],
-    value: 0
+    value: 0,
   },
   {
     name: 'Family',
     color: blue[SHADE],
-    value: 0
+    value: 0,
   },
   {
     name: 'Carreer',
     color: pink[SHADE],
-    value: 0
+    value: 0,
   },
   {
     name: 'Personal development',
     color: yellow[SHADE],
-    value: 0
+    value: 0,
   },
 
   {
     name: 'Partner',
     color: purple[SHADE],
-    value: 0
+    value: 0,
   },
   {
     name: 'Spirituality',
     color: deepOrange[SHADE],
-    value: 0
+    value: 0,
   },
   {
     name: 'Fun',
     color: brown[SHADE],
-    value: 0
+    value: 0,
   },
   {
     name: 'Environment',
     color: green[SHADE],
-    value: 0
+    value: 0,
   },
   {
     name: 'Finances',
     color: cyan[SHADE],
-    value: 0
+    value: 0,
   },
   {
     name: 'Friends',
     color: amber[SHADE],
-    value: 0
+    value: 0,
   },
   {
     name: 'Social',
     color: indigo[SHADE],
-    value: 0
-  }
-];
+    value: 0,
+  },
+]
