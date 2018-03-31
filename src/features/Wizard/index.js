ad51d1cc94dd255a3fb7bca8e5219de19da61b43
Rohov <rogov.dmitry@gmail.com>
2018-03-31 16:38:55 +0300

    🎨

--- src/features/Wizard/index.js
+++ src/features/Wizard/index.js
@@ -1,23 +1,23 @@
-import { compose, withProps, withState, withStateHandlers } from 'recompose';
+import { compose, withProps, withState, withStateHandlers } from 'recompose'
 
-import AppBar from 'features/AppBar';
-import { BLOCKS } from 'common/constants';
-import { Link } from 'react-router-dom';
-import Progress from 'features/Progress';
-import Question from 'features/Question';
-import React from 'react';
-import { encodeStateToString } from 'common/helpers';
+import AppBar from 'features/AppBar'
+import { BLOCKS } from 'common/constants'
+import { Link } from 'react-router-dom'
+import { Progress } from 'semantic-ui-react'
+import Question from 'features/Question'
+import React from 'react'
+import { encodeStateToString } from 'common/helpers'
 
 const StepButton = ({ ...props }) => (
   <button
     style={{
       cursor: 'pointer',
       width: '60px',
-      height: '60px'
+      height: '60px',
     }}
     {...props}
   />
-);
+)
 
 const Component = ({
   match,
@@ -27,16 +27,21 @@ const Component = ({
   setIndex,
   isFinished,
   isLastStep,
-  updateBlockWithValue
+  updateBlockWithValue,
 }) => (
   <React.Fragment>
     <div
       style={{
         height: '100vh',
         backgroundColor: index < blocks.length ? blocks[index].color : 'white',
-        transition: 'background-color 0.8s'
-      }}
-    >
+        transition: 'background-color 0.8s',
+      }}>
+      <Progress
+        success
+        progress
+        percent={Math.floor(100 * index / blocks.length)}
+      />
+
       <AppBar />
       <br />
       <br />
@@ -44,15 +49,17 @@ const Component = ({
       <React.Fragment>
         <br />
         <br />
-        <Progress blocks={blocks} index={index} />
         {isFinished ? (
-          <h1 style={{ textAlign: 'center', fonstSize: '60px', color: 'white' }}>DONE</h1>
+          <h1
+            style={{ textAlign: 'center', fonstSize: '60px', color: 'white' }}>
+            DONE
+          </h1>
         ) : (
           <Question
             block={blocks[index]}
             onChange={value => {
-              updateBlockWithValue({ index, value });
-              setIndex(index + 1);
+              updateBlockWithValue({ index, value })
+              setIndex(index + 1)
             }}
           />
         )}
@@ -60,24 +67,26 @@ const Component = ({
           <div
             style={{
               display: 'flex',
-              justifyContent: 'space-between'
-            }}
-          >
-            <StepButton onClick={() => setIndex(index - 1)} disabled={index === 0}>
+              justifyContent: 'space-between',
+            }}>
+            <StepButton
+              onClick={() => setIndex(index - 1)}
+              disabled={index === 0}>
               prev
             </StepButton>
             <StepButton
               disabled={isFinished}
               onClick={() => {
-                updateBlockWithValue({ index, value: 0 });
-                setIndex(index + 1);
-              }}
-            >
+                updateBlockWithValue({ index, value: 0 })
+                setIndex(index + 1)
+              }}>
               skip
             </StepButton>
             {isLastStep ? (
               <Link to={`/results/${encodeStateToString({ blocks })}`}>
-                <StepButton onClick={() => setIndex(index + 1)}>FINISH</StepButton>
+                <StepButton onClick={() => setIndex(index + 1)}>
+                  FINISH
+                </StepButton>
               </Link>
             ) : (
               <StepButton onClick={() => setIndex(index + 1)}>NEXT</StepButton>
@@ -87,25 +96,28 @@ const Component = ({
       </React.Fragment>
     </div>
   </React.Fragment>
-);
+)
 
 export default compose(
   withState('blocks', 'setBlocks', BLOCKS),
   withStateHandlers(null, {
-    updateBlockWithValue: (state, { setBlocks, blocks }) => ({ index, value }) => {
+    updateBlockWithValue: (state, { setBlocks, blocks }) => ({
+      index,
+      value,
+    }) => {
       setBlocks([
         ...blocks.slice(0, index),
         {
           ...blocks[index],
-          value: value
+          value: value,
         },
-        ...blocks.slice(index + 1, blocks.length)
-      ]);
-    }
+        ...blocks.slice(index + 1, blocks.length),
+      ])
+    },
   }),
   withState('index', 'setIndex', 0),
   withProps(props => ({
     isLastStep: props.index === props.blocks.length - 1,
-    isFinished: props.index >= props.blocks.length - 1
-  }))
-)(Component);
+    isFinished: props.index >= props.blocks.length - 1,
+  })),
+)(Component)
