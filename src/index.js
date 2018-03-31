5159ea949ca58a3d6bf8c1181648975452993c6a
Rohov <rogov.dmitry@gmail.com>
2018-03-31 16:06:51 +0300

    ➖ remove usage of typography.js

--- src/index.js
+++ src/index.js
@@ -3,11 +3,6 @@ import 'index.css'
 import App from 'App.js'
 import React from 'react'
 import ReactDOM from 'react-dom'
-import Typography from 'typography'
 import registerServiceWorker from 'registerServiceWorker.js'
-import typographyTheme from 'typography-theme-lincoln'
-
-const typography = new Typography(typographyTheme)
-// typography.injectStyles();
 
 ReactDOM.render(<App />, document.getElementById('root'))
