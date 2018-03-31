00e9ddba8bdc928d654100dca2675c57f85b2a8e
Rohov <rogov.dmitry@gmail.com>
2018-03-31 16:04:39 +0300

    🎨 first button with semantic ui

--- src/features/Welcome/index.js
+++ src/features/Welcome/index.js
@@ -1,6 +1,6 @@
-import { Link } from 'react-router-dom';
-import React from 'react';
-
+import { Button } from 'semantic-ui-react'
+import { Link } from 'react-router-dom'
+import React from 'react'
 export default () => (
   <div>
     <h1 style={{ textAlign: 'center', fontSize: '60px' }}>Well hello</h1>
@@ -8,8 +8,8 @@ export default () => (
     <h3 style={{ textAlign: 'center' }}>Whant to balance your life?</h3>
     <div style={{ textAlign: 'center' }}>
       <Link to={'/quiz'}>
-        <button style={{ cursor: 'pointer' }}>Start</button>
+        <Button>Start</Button>
       </Link>
     </div>
   </div>
-);
+)
