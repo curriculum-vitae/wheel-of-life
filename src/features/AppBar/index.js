d55d04a8a26c9239ef36ad99912601df6cbc3013
Rohov <rogov.dmitry@gmail.com>
2018-04-02 15:55:12 +0300

    ➕

--- src/features/AppBar/index.js
+++ src/features/AppBar/index.js
@@ -1,12 +1,12 @@
 import { Link } from 'react-router-dom'
 import React from 'react'
-
+import { grey } from 'utils/colors'
 export default ({ color = 'white' }) => (
   <div
     style={{
       height: '64px',
       padding: '12px 20px',
-      borderBottom: '1px solid white',
+      borderBottom: `1px solid ${grey[200]}`,
     }}>
     <Link to={'/'}>
       <h2
--- src/features/Question/index.js
+++ src/features/Question/index.js
@@ -1,12 +1,18 @@
-import { Button } from 'semantic-ui-react'
+import { Button, List } from 'semantic-ui-react'
+
 import React from 'react'
 import { times } from 'lodash/fp'
 
+const DUMMY_SUBQUESTIONS = [
+  'How meaninful your something?',
+  'What is the weather?',
+  'How much time do you pur in this shpere?',
+  'Do you plan it?',
+  'How much better you can do it?',
+]
+
 export default ({ block, onChange = () => {} }) => (
-  <div
-    style={{
-      padding: '50px',
-    }}>
+  <div>
     <h2
       style={{
         textAlign: 'center',
@@ -16,13 +22,22 @@ export default ({ block, onChange = () => {} }) => (
       }}>
       {block.name}
     </h2>
-
+    <List divided relaxed>
+      {DUMMY_SUBQUESTIONS.map(str => (
+        <List.Item key={str} color={'white'}>
+          <List.Icon name={'question'} size={'large'} verticalAlign="middle" />
+          <List.Content>
+            <List.Header>{str}</List.Header>
+          </List.Content>
+        </List.Item>
+      ))}
+    </List>
     <div
       style={{
         display: 'flex',
         justifyContent: 'center',
       }}>
-      <Button.Group size={'large'}>
+      <Button.Group size={'large'} fluid>
         {times(index => (
           <Button
             primary={block.value === index + 1}
