f37b6f0ecc591e8b7faacedf943f9f5c8d5e1f11
Kyrylo Zapylaiev <zapylaev@gmail.com>
2018-04-02 23:14:03 +0300

    Clean up, change colors

--- src/features/Wheel/index.js
+++ src/features/Wheel/index.js
@@ -6,36 +6,10 @@ import {
 } from 'lodash/fp'
 import React from 'react'
 import { setDisplayName } from 'recompose'
-import { BLOCKS } from 'utils/constants'
 import BarChart from './BarChart'
-
-const mapPropsToQueries = () => []
-
-const convertBlocksToWindRoseData = blocks => {
-  const STEP = 360 / blocks.length
-  return flow(
-    map(block => ({
-      label: block.name,
-      angle: `${STEP * blocks.indexOf(block)} - ${STEP *
-      (blocks.indexOf(block) + 1)}`,
-      value: block.value,
-    })),
-  )(blocks)
-}
-
-const SIZE = 500
-
-const getColor = label =>
-  flow(find(block => block.name === label), block => block.color)(BLOCKS)
 
 const Component = ({ blocks }) => (
   <BarChart data={blocks} />
 )
-
-const mapStateToProps = state => {
-  return {}
-}
-
-const mapDispatchToProps = {}
 
 export default compose(setDisplayName('DefaultComponentName'))(Component)
