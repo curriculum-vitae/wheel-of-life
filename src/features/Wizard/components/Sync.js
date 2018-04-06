4a55f199e5e52922e95b33a80f0abddb580b574d
Rohov <rogov.dmitry@gmail.com>
2018-04-06 15:25:21 +0300

    🔄

--- src/features/Wizard/components/Sync.js
+++ src/features/Wizard/components/Sync.js
@@ -3,7 +3,7 @@ import { Redirect } from 'react-router-dom'
 import { encodeStateToString } from 'utils/helpers'
 import { isInSync } from 'features/Wizard/helpers'
 
-export default ({ match, blocks, index }) =>
+export const WizardSync = ({ match, blocks, index }) =>
   isInSync({ match, blocks, index }) ? null : (
     <Redirect to={`/quiz/${encodeStateToString({ blocks, index })}`} />
   )
