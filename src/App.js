03d932d6dc0c71bbee2e016076d647f354bc6466
Rohov <rogov.dmitry@gmail.com>
2018-04-06 15:26:28 +0300

    🔄 clean up

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
 import 'App.css'
 
-import {
-  Link,
-  Redirect,
-  Route,
-  BrowserRouter as Router,
-  Switch,
-} from 'react-router-dom'
 import React, { Component } from 'react'
-import { decodeStateFromString, encodeStateToString } from 'utils/helpers'
+import { Route, BrowserRouter as Router, Switch } from 'react-router-dom'
 
-import { BLOCKS } from 'utils/constants'
 import { Results } from 'features/Results'
 import { Welcome } from 'features/Welcome'
-import Wheel from 'features/Wheel'
 import { Wizard } from 'features/Wizard'
-import { compose } from 'lodash/fp'
-import logo from 'logo.svg'
-
-const Container = ({ children }) => <div style={{}} children={children} />
 
 class App extends Component {
   render() {
