3141e43ad7622b77e5e734c090fb95f52aeca814
Rohov <rogov.dmitry@gmail.com>
2018-04-07 15:15:44 +0300

    🔄  refactor; remove transform attr

--- src/features/Wheel/index.js
+++ src/features/Wheel/index.js
@@ -1,34 +1,36 @@
 import * as d3 from 'd3'
+
 import { compose, lifecycle, setDisplayName, withProps } from 'recompose'
 
 import { RefsStore } from '../../utils/refsUtils'
 import { WheelChart } from './components/WheelChart'
 
 const createChart = ({ refs, width = 500, height = 500, blocks }) => {
-  const maxValue = 10
+  const MAX_VALUE = 10
 
   const barHeight = height / 2 - 100
+
+  const r = width / 2
+  const names = blocks.map(d => d.name)
+  const countOfBlocks = names.length
+  const theta = 2 * Math.PI / names.length
 
   const svg = d3
     .select(refs.svg)
     .append('svg')
     .attr('preserveAspectRatio', 'xMinYMin meet')
-    .attr('viewBox', `0 0 ${width} ${height}`)
+    .attr('viewBox', `-280 -280 ${width} ${height}`)
     .classed('svg-content', true)
     .append('g')
-    .attr('transform', `translate(${width / 2},${height / 2})`)
 
   const barScale = d3
     .scaleLinear()
-    .domain([0, maxValue])
+    .domain([0, MAX_VALUE])
     .range([0, barHeight])
-
-  const keys = blocks.map(d => d.name)
-  const numBars = keys.length
 
   const x = d3
     .scaleLinear()
-    .domain([0, maxValue])
+    .domain([0, MAX_VALUE])
     .range([0, -barHeight])
 
   svg
@@ -36,9 +38,7 @@ const createChart = ({ refs, width = 500, height = 500, blocks }) => {
     .data(x.ticks(10))
     .enter()
     .append('circle')
-    .attr('r', function(d) {
-      return barScale(d)
-    })
+    .attr('r', d => barScale(d))
     .style('fill', 'none')
     .style('stroke', '#a7aaa9')
     .style('stroke-dasharray', '1,3')
@@ -46,8 +46,8 @@ const createChart = ({ refs, width = 500, height = 500, blocks }) => {
 
   const arc = d3
     .arc()
-    .startAngle((d, i) => i * 2 * Math.PI / numBars)
-    .endAngle((d, i) => (i + 1) * 2 * Math.PI / numBars)
+    .startAngle((d, i) => i * 2 * Math.PI / countOfBlocks)
+    .endAngle((d, i) => (i + 1) * 2 * Math.PI / countOfBlocks)
     .innerRadius(0)
 
   const segments = svg
@@ -55,17 +55,15 @@ const createChart = ({ refs, width = 500, height = 500, blocks }) => {
     .data(blocks)
     .enter()
     .append('path')
-    .each(d => {
-      d.outerRadius = 0
-    })
+    .each(d => (d.outerRadius = 0))
     .style('fill', d => d.color)
     .attr('d', arc)
 
   segments
     .transition()
-    .ease(d3.easeCubic)
-    .duration(800)
-    .delay((d, i) => i * 100)
+    // .ease(d3.easeCubic)
+    .duration(1)
+    .delay((d, i) => i * 1)
     .attrTween('d', (d, index) => {
       const i = d3.interpolate(d.outerRadius, barScale(+d.value))
       return t => {
@@ -84,13 +82,13 @@ const createChart = ({ refs, width = 500, height = 500, blocks }) => {
 
   svg
     .selectAll('line')
-    .data(keys)
+    .data(names)
     .enter()
     .append('line')
     .attr('y2', -barHeight - 20)
     .style('stroke', '#696969')
     .style('stroke-width', '.5px')
-    .attr('transform', (d, i) => 'rotate(' + i * 360 / numBars + ')')
+    .attr('transform', (d, i) => 'rotate(' + i * 360 / countOfBlocks + ')')
 
   const labelRadius = barHeight * 1.025
 
@@ -105,9 +103,6 @@ const createChart = ({ refs, width = 500, height = 500, blocks }) => {
       `m0 ${-labelRadius} a${labelRadius} ${labelRadius} 0 1,1 -0.01 0`,
     )
 
-  const theta = 2 * Math.PI / keys.length
-  const r = 250
-
   console.log('blocks', blocks)
   labels
     .selectAll('text')
@@ -121,7 +116,11 @@ const createChart = ({ refs, width = 500, height = 500, blocks }) => {
       d.outerRadius = 0
     })
     .style('fill', d => d.color)
-    .attr('transform', (d, i) => `translate(${r * Math.cos(i * theta)}, ${r * Math.sin(i * theta)})`)
+    .attr(
+      'transform',
+      (d, i) =>
+        `translate(${r * Math.cos(i * theta)}, ${r * Math.sin(i * theta)})`,
+    )
     .text(block => block.name.toUpperCase())
 }
 
