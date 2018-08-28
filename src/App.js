0aea831dc6e2645ec1f0b055d97bbbb68ab93f0b
Rohov <rogov.dmitry@gmail.com>
2018-08-28 14:24:31 +0300

    ⬆️

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import 'App.css'
 import React, { Component } from 'react'
 import { Route, BrowserRouter as Router, Switch } from 'react-router-dom'
 
+import { CssBaseline } from '@material-ui/core'
 import { Results } from 'features/Results'
 import { Welcome } from 'features/Welcome'
 import { Wizard } from 'features/Wizard'
@@ -11,6 +12,7 @@ class App extends Component {
   render() {
     return (
       <React.Fragment>
+        <CssBaseline />
         <Router>
           <Switch>
             <Route path={'/'} exact={true} component={Welcome} />
